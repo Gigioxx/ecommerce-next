330734313cc188d96f0ac1eb55a66e464df10f39
Gigiox <guillermo.casanova.b@gmail.com>
2022-10-15 23:50:45 -0300

    ✨ Added context for products page

--- pages/product/[slug].js
+++ pages/product/[slug].js
@@ -8,10 +8,12 @@ import {
 
 import { client, urlFor } from '../../lib/client';
 import { Product } from '../../components';
+import { useStateContext } from '../../context/StateContext';
 
 const ProductDetails = ({ product, products }) => {
   const { image, name, details, price } = product;
   const [index, setIndex] = useState(0);
+  const { decreaseQuantity, increaseQuantity, qty, onAdd } = useStateContext();
 
   return (
     <div>
@@ -36,6 +38,7 @@ const ProductDetails = ({ product, products }) => {
             ))}
           </div>
         </div>
+
         <div className='product-detail-desc'>
           <h1>{name}</h1>
           <div className='reviews'>
@@ -54,17 +57,21 @@ const ProductDetails = ({ product, products }) => {
           <div className='quantity'>
             <h3>Quantity: </h3>
             <p className='quantity-desc'>
-              <span className='minus' onClick=''>
+              <span className='minus' onClick={decreaseQuantity}>
                 <AiOutlineMinus />
               </span>
-              <span className='num'>0</span>
-              <span className='plus' onClick=''>
+              <span className='num'>{qty}</span>
+              <span className='plus' onClick={increaseQuantity}>
                 <AiOutlinePlus />
               </span>
             </p>
           </div>
           <div className='buttons'>
-            <button type='button' className='add-to-cart' onClick=''>
+            <button
+              type='button'
+              className='add-to-cart'
+              onClick={() => onAdd(product, qty)}
+            >
               Add to Cart
             </button>
             <button type='button' className='buy-now' onClick=''>
